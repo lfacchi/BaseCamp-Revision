547aa64162b44184d1f2e0951b6e8473b71b134d
Daniel Tavares <iazegamer16@gmail.com>
2022-02-09 18:16:20 -0300

    Update correcao.sh

--- C01/correcao.sh
+++ C01/correcao.sh
@@ -1,6 +1,14 @@
 clear
 echo '\n Realizando teste da norminette -R\n'
-norminette -R CheckForbiddenSourceHeader
+norminette -R CheckForbiddenSourceHeader ex00/ft_ft.c
+norminette -R CheckForbiddenSourceHeader ex01/ft_ultimate_ft.c
+norminette -R CheckForbiddenSourceHeader ex02/ft_swap.c
+norminette -R CheckForbiddenSourceHeader ex03/ft_div_mod.c
+norminette -R CheckForbiddenSourceHeader ex04/ft_ultimate_div_mod.c
+norminette -R CheckForbiddenSourceHeader ex05/ft_putstr.c
+norminette -R CheckForbiddenSourceHeader ex06/ft_strlen.c
+norminette -R CheckForbiddenSourceHeader ex07/ft_rev_int_tab.c
+norminette -R CheckForbiddenSourceHeader ex08/ft_sort_int_tab.c
 echo '\nTeste da norminette concluido\n'
 echo '\nRealizando teste de GCC\n'
 mkdir test
